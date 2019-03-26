95b9dba5698d3d31fff5dc057f74b5a8c89d269c
jimmyboy15 <37394516+jimmyboy15@users.noreply.github.com>
2019-03-27 03:00:05 +0800

    UpdatedJSquiz

--- JavaScriptQuiz/quiz.js
+++ JavaScriptQuiz/quiz.js
@@ -2,6 +2,9 @@ function done() {
 
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
 	var correct = 0;
 
 	if (question1 == "Java Script") {
@@ -11,7 +14,37 @@ function done() {
 	if (question2 == "Document Object Model") {
 		correct++;
 	}
-
+	
+	if (question3 == "A programming interface") {
+		correct++;
+	}
+	
+	if (question4 == "the document as nodes and objects") {
+		correct++;
+	  }
+	
+	if (question4 == "the document as nodes and objects") {
+		correct++;
+	}
+	
+	if (question5 == "It can add/modify/remove") {
+		correct++;
+	}
+	
+	var messages = ["Good job!", "That's just okay", "You really need to do better!"];
+	
+	var range;
+	if (correct < 1) {
+		range = 2;
+	}
+	if (correct > 0 && correct < 5) {
+		range = 1;
+	}
+	if (correct > 3){
+		range = 0;
+	}
+	
 	document.getElementById("after_submit").style.visibility = "visible";
-	document.getElementById("numberOfCorrect").innerHTML = "Your got " + correct + " correct.";
+	alert(document.getElementById("message").innerHTML = messages[range]);
+	alert(document.getElementById("numberOfCorrect").innerHTML = "You got " + correct + " correct answers.");
 }
