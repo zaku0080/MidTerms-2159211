27b208b5139b9a38afe19b98f83053d746f38710
2152028 <44063487+2152028@users.noreply.github.com>
2019-03-29 23:37:42 +0800

    Crossword

---
+++ crossword/crossword.js
@@ -0,0 +1,12 @@
+var add;
+
+var buttons = function() {
+	mybuttons = document.getElementById("complete");
+	apple = document.createElement('button');
+}
+
+function myFunction() {
+	document.getElementsByName("item").value = "";
+}
+
+
