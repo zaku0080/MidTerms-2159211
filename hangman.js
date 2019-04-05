28633026892c5daf883ca306f4c09cc560b7ea48
2151789 <34244527+2151789@users.noreply.github.com>
2019-04-06 06:50:48 +0800

    added the alert in javascript

---
+++ hangman.js
@@ -0,0 +1,234 @@
+window.onload = function () {
+
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  var categories;         // Array of topics
+  var getHint ;          // Word getHint
+  var word ;              // Selected word
+  var guess ;             // Geuss
+  var geusses = [ ];      // Stored geusses
+  var lives ;             // Lives
+  var counter ;           // Count correct geusses
+  var space;             // Number of spaces in word '-'
+  var list;
+
+  // Get elements
+  var showLives = document.getElementById("mylives");
+  var getHint = document.getElementById("hint");
+  var showClue = document.getElementById("clue");
+
+
+
+  // create alphabet ul
+  var buttons = function () {
+    myButtons = document.getElementById('buttons');
+    letters = document.createElement('ul');
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.setAttribute('id', 'alphabet');
+      list = document.createElement('li');
+      list.setAttribute('class', 'letter');
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
+    }
+  }
+
+
+  // Select Catagory
+  var selectCat = function () {
+    if (chosenCategory === categories[0]) {
+      catagoryname.innerHTML = "CSS Interactive Learning";
+    }
+  }
+
+  // Create geusses ul
+   result = function () {
+    var wordHolder = document.getElementById('hold');
+    correct = document.createElement('ul');
+
+    for (var i = 0; i < word.length; i++) {
+      correct.setAttribute('id', 'my-word');
+      guess = document.createElement('li');
+      guess.setAttribute('class', 'guess');
+      if (word[i] === "-") {
+        guess.innerHTML = "-";
+        space = 1;
+
+      } else {
+        guess.innerHTML = "_";
+      }
+      geusses.push(guess);
+      wordHolder.appendChild(correct);
+      correct.appendChild(guess);
+    }
+  }
+
+  // Show lives
+   comments = function () {
+    showLives.innerHTML = "You have " + lives + " lives";
+    if (lives < 1) {
+      alert(showLives.innerHTML = "Game Over");
+      var unclick = document.getElementById('alphabet');
+      unclick.classList.add("unclick");
+
+    }
+    for (var i = 0; i < geusses.length; i++) {
+      if (counter + space === geusses.length) {
+        var unclick = document.getElementById('alphabet');
+        unclick.classList.add("unclick");
+        alert(showLives.innerHTML = "You Win!");
+      }
+    }
+  }
+
+      // Animate man
+  var animate = function () {
+    var drawMe = lives ;
+    drawArray[drawMe]();
+  }
+
+
+   // Hangman
+  canvas =  function(){
+
+    var myStickman = document.getElementById("stickman");
+    context = myStickman.getContext('2d');
+    context.beginPath();
+    context.strokeStyle = "#FFA500";
+    context.lineWidth = 2;
+  };
+
+    head = function(){
+      myStickman = document.getElementById("stickman");
+      context = myStickman.getContext('2d');
+      context.beginPath();
+      context.arc(60, 25, 10, 0, Math.PI*2, true);
+      context.stroke();
+    }
+
+  draw = function($pathFromx, $pathFromy, $pathTox, $pathToy) {
+
+    context.moveTo($pathFromx, $pathFromy);
+    context.lineTo($pathTox, $pathToy);
+    context.stroke();
+    context.lineWidth = 3;
+}
+
+   frame1 = function() {
+     draw (0, 150, 150, 150);
+   };
+
+   frame2 = function() {
+     draw (10, 0, 10, 600);
+   };
+
+   frame3 = function() {
+     draw (0, 5, 70, 5);
+   };
+
+   frame4 = function() {
+     draw (60, 5, 60, 15);
+   };
+
+   torso = function() {
+     draw (60, 36, 60, 70);
+   };
+
+   rightArm = function() {
+     draw (60, 46, 100, 50);
+   };
+
+   leftArm = function() {
+     draw (60, 46, 20, 50);
+   };
+
+   rightLeg = function() {
+     draw (60, 70, 100, 100);
+   };
+
+   leftLeg = function() {
+     draw (60, 70, 20, 100);
+   };
+
+  drawArray = [rightLeg, leftLeg, rightArm, leftArm,  torso,  head, frame4, frame3, frame2, frame1];
+
+
+  // OnClick Function
+   var check = function () {
+    list.onclick = function () {
+      var geuss = (this.innerHTML);
+      this.setAttribute("class", "active");
+      //this.addClass('class', 'green');
+      //this.onclick = null;
+      for (var i = 0; i < word.length; i++) {
+        if (word[i] === geuss) {
+          geusses[i].innerHTML = geuss;
+          counter += 1;
+          this.classList.remove("active");
+          this.classList.add("green");
+        }
+      }
+      var j = (word.indexOf(geuss));
+      if (j === -1) {
+        lives -= 1;
+        comments();
+        animate();
+      } else {
+        comments();
+      }
+    }
+  }
+
+
+  // Play
+  play = function () {
+    categories = [
+        ["attribute selector", "universal selector", "pseudo element", "adjacent sibling combinator", "sibling combinator", "descendant combinator",
+		"class selector","id selector","cascading style sheet","type selector"]
+    ]
+
+    chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+    word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+    word = word.replace(/\s/g, "-");
+    console.log(word);
+    buttons();
+
+    geusses = [ ];
+    lives = 10;
+    counter = 0;
+    space = 0;
+    result();
+    comments();
+    selectCat();
+    canvas();
+  }
+
+  play();
+
+  // Hint
+
+    hint.onclick = function() {
+
+      hints = [
+        [".info", "#info", "*", "&", "Once managed by Phil Brown", "2013 FA Cup runners up", "Gazza's first club"]
+    ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(word);
+    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+  };
+
+   // Reset
+
+  document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
+}
