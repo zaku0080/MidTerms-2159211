63cd20dfb7fc6f66405d6b6cca4642e8d47498c2
RyanChristopher <ryantot03@gmail.com>
2019-04-06 22:29:39 +0800

    Fix bugs, added questions, fixed case sensitive

--- JavaScriptQuiz/quiz.js
+++ JavaScriptQuiz/quiz.js
@@ -9,6 +9,12 @@ function done() {
 	var question7 = document.quiz.question7.value;
 	var question8 = document.quiz.question8.value;
 	var question9 = document.quiz.question9.value;
+	var question10 = document.quiz.question10.value;
+	var question11 = document.quiz.question11.value;
+	var question12 = document.quiz.question12.value;
+	var question13 = document.quiz.question13.value;
+	var question14 = document.quiz.question14.value;
+	var question15 = document.quiz.question15.value;
 	var correct = 0;
 
 	if (question1 == "JavaScript") {
@@ -55,20 +61,38 @@ function done() {
 	if (question7 == "Document Object Model") {
 		correct++;
 	}
-	
-	if (question8 == "client side and admin side") {
+
+	if (question8 == "adoptNode()") {
 		correct++;
 	}
-	
-	if (question8 == "client side and the admin side") {
+
+	if (question9 == "Nodes") {
 		correct++;
 	}
-	
-	if (question9 == "Anchor, Applet, Attr") {
+
+	if (question10 == "Storage") {
 		correct++;
 	}
-	
-	
+
+	if (question11 == "normalize()") {
+		correct++;
+	}
+
+	if (question12 == "dispatchStyle") {
+		correct++;
+	}
+
+	if (question13 == "removeAttribute()") {
+		correct++;
+	}
+
+	if (question14 == "adoptNode()") {
+		correct++;
+	}
+
+	if (question15 == "appendChild") {
+		correct++;
+	}
 	document.getElementById("after_submit").style.visibility = "visible";
 	alert(document.getElementById("numberOfCorrect").innerHTML = "You got " + correct + " correct answers.");
 }
